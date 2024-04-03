ae19084cc4b55fe3d9c01056878a8f5e9465b5f1
bygerman <by.german@outlook.com>
2024-04-02 22:08:59 -0500

    Add input validation to TaskController.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -16,6 +17,15 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:5|max:20',
+            'description' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
         $task = Task::create($request->all());
         return response()->json($task, 201);
     }
